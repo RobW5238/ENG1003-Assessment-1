642db201ef98dfc8fe5c407f6fca3408d0b18de8
RobW5238 <robert.weatherall5238@gmail.com>
2019-04-07 20:58:18 +1000

    Fixed issue causing the code to skip to the end of the function w/fflush

--- Encryption.c
+++ Encryption.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void rotationCipher(char* Text, int key);
+//void rotationCipher(char* Text, int key);
 
 int main(void)
 {
--- encrypt_decrypt_combo.c
+++ encrypt_decrypt_combo.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+void RtationCipher(char* Text, int key);
 
 int main()
 {
   printf("rotation cipher software v3.4.2\n\n");
   printf("select a function\n\n");
-  printf("c) encrypt message\n\n");
+  printf("a) encrypt message\n\n");
   printf("b) decrypt message\n\n");
   printf("c) terminate program\n\n");
 
@@ -17,28 +18,63 @@ int main()
   char c;
   scanf ("%c", &x);
 
+
+
+
+
+
   while (x>='a' && x<='c')
     {
       switch (x)
       {
-        void rotationCipher(char* Text, int key);
         case 'a':
-          printf("selected a\n");
+          printf("\nselected encryption\n");
+          exRotationCipher();
           break;
         case 'b':
           printf("selected b\n");
+          return 0;
           break;
         case 'c':
           printf("terminating program\n");
           return 0;
           break;
-
-
-
-      }
-
-      return 0;
-
+        }
     }
+  printf("\ninvalid input\n");
   return 0;
+}
+
+
+int exRotationCipher()
+{
+  fflush(stdin);
+  int key;
+  char Text[101];
+  printf("\nrotation cipher encryption software\n\n" );
+  printf(" .\n");
+  printf(" .\n");
+  printf(" .\n\n");
+  printf("Enter the message to be encrypted;\n\n");
+  fgets(Text, sizeof(Text), stdin);
+  printf("\nenter the value of the encryption key;\n\n" );
+  scanf("%d",&key);
+  printf("\nthe encrypted message is:\n\n" );
+  rotationCipher(Text, key);
+  system("pause");
+}
+
+void rotationCipher(char* Text, int key)
+{
+  int i=0;
+  int val;
+  char encTxt;
+  while(Text[i] != '\0' && strlen(Text)-1 > i)
+    {
+      val = ((int)Text[i] + key);
+      encTxt = (char)(val);
+      printf ("%c", encTxt);
+      i++;
+    }
+  printf("\n\n" );
 }
